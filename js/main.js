26484b7ec75121c6e5e0dbc79f832ca60d974e3e
Ralph Gerard Ramirez <86941342+ralphgerard@users.noreply.github.com>
2022-10-31 17:33:35 +0800

    testing animation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 // Animate Smoooth Scroll
 $('#view-work').on('click', function() {
     const images = $('#images').position().top;
+    
 
     $('html, body').animate({
        scrollTop: images 
     }, 900);
+    console.log("It works");
+
+    
 });
