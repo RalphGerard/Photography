d7cdfa436f33c9031e3086c59e60c62ff7828638
Ralph Gerard Ramirez <86941342+ralphgerard@users.noreply.github.com>
2022-10-31 16:25:21 +0800

    Trying to fix the main.js animation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // Animate Smoooth Scroll
 $('#view-work').on('click', function() {
-    const images = $ ('#images').position().top;
+    const images = $('#images').position().top;
 
-    $('html,body').animate({
+    $('html, body').animate({
        scrollTop: images 
     }, 900);
 });
